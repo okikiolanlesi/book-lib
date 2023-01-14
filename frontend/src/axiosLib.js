a4d418fb2235ab815bd379112bb3ade65ba5ff62
okikiolanlesi <osunronbiolanlesi@gmail.com>
2023-01-14 16:45:31 +0100

    changed baseURL in axiosLib file

--- frontend/src/axiosLib.js
+++ frontend/src/axiosLib.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosLib = axios.create({
-  baseURL: "http://localhost:8000/api/v1",
+  baseURL: "https://book-lib-api-backend.onrender.com/api/v1",
   headers: {
     "Content-Type": "application/json",
   },
